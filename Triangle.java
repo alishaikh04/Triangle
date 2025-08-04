4f66a71566365418b7568ee982b8507ed27f6443
alishaikh04 <alishaikhzk@gmail.com>
2025-08-04 08:18:52 -0700

    Update Triangle.java

--- Triangle.java
+++ Triangle.java
@@ -7,21 +7,24 @@ public class Triangle {
     // Default constructor
     public Triangle() 
     {
-        this(1, 1, 1);
+        this.a = 1.0;
+        this.b = 1.0;
+        this.c = 1.0;
     }
 
     // One side constructor (equilateral)
     public Triangle(double side) 
     {
         this.a = side;
-         this.b = side;
-          this.c = side;
+        this.b = side;
+        this.c = side;
     }
 
     // Two sides constructor (isosceles)
     public Triangle(double a, double b) 
     {
-        this(a, b, Math.sqrt(a * a + b * b)); // Assume right triangle by default
+        this(a, b, Math.sqrt(a * a + b * b)); 
+       // Assume right triangle by default
     }
 
     // Three sides constructor
@@ -33,20 +36,25 @@ public class Triangle {
     }
 
     // Copy constructor
-    public Triangle(Triangle other) {
+    public Triangle(Triangle other)
+   {
         this.a = other.a;
         this.b = other.b;
         this.c = other.c;
     }
 
-    public double perimeter() {
+    public double perimeter() 
+    {
         return a + b + c;
     }
 
-    public boolean isRightAngle() {
+    public boolean isRightAngle() 
+   {
         double[] sides = {a, b, c};
+
         java.util.Arrays.sort(sides);
         return Math.abs((sides[0] * sides[0] + sides[1] * sides[1]) - (sides[2] * sides[2])) < 1e-9;
+
     }
 
     public static int Count() {
