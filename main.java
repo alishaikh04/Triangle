c973758848c9391f4858b90fc45f62e8ca75d9af
alishaikh04 <alishaikhzk@gmail.com>
2025-08-06 06:15:20 -0700

    Update main.java

--- main.java
+++ main.java
@@ -1,31 +1,25 @@
-public class main {
+public class main{
     public static void main(String[] args) {
-        Triangle t1 =  new Triangle();
+        Triangle t1 = new Triangle();                        // Default: all sides = 1
+        Triangle t2 = new Triangle(5.0);                     // Equilateral
+        Triangle t3 = new Triangle(3.0, 4.0);                // Right-angled (Pythagorean triple)
+        Triangle t4 = new Triangle(7.0, 24.0, 25.0);         // Right-angled triangle
+        Triangle t5 = new Triangle(t2);                      // Copy of t2
 
-        Triangle t2 = new Triangle(3);
+        System.out.println("T1:\n" + t1);
+        System.out.println("Is T1 right-angled? " + t1.isRightAngle());
 
-        Triangle t3 = new Triangle(3,4);
+        System.out.println("T2:\n" + t2);
+        System.out.println("Is T2 right-angled? " + t2.isRightAngle());
 
-        Triangle t4 = new Triangle(3,4,5);
+        System.out.println("T3:\n" + t3);
+        System.out.println("Is T3 right-angled? " + t3.isRightAngle());
 
-        Triangle  t5 = new Triangle(t4);
+        System.out.println("T4:\n" + t4);
+        System.out.println("Is T4 right-angled? " + t4.isRightAngle());
 
-        System.out.println(t1);
-        System.out.println("perimeter : " + t1.perimeter());
-        System.out.println("Right angle : " + t1.isRightAngle());
-
-        System.out.println("perimeter : " + t2.perimeter());
-        System.out.println("Right angle : " + t2.isRightAngle());
-
-
-        System.out.println("perimeter : " + t3.perimeter());
-        System.out.println("Right angle : " + t3.isRightAngle());
-
-                System.out.println("perimeter : " + t4.perimeter());
-        System.out.println("Right angle : " + t4.isRightAngle());
-
-                System.out.println("perimeter : " + t5.perimeter());
-        System.out.println("Right angle : " + t5.isRightAngle());
-
+        System.out.println("T5 (copy of T2):\n" + t5);
+        
+        System.out.println("Total Triangle objects created: " + Triangle.count);
     }
 }
